b1040a6e048d35c5fc8d3bf41adc49273c763ebe
soumya <soumyakole@gmail.com>
2024-04-07 16:48:12 -0500

    first-cut

---
+++ Word_Index.py
@@ -0,0 +1,13 @@
+import collections
+def keyword_index(docs):
+    # implement this
+    d = collections.defaultdict(lambda:collections.defaultdict(int))
+    for i, v in enumerate(docs):
+        for w in v.split(' '):
+            d[w][i] += 1
+    return d
+
+docs = ["Hello world", "world of python", "python is a snake"]
+print(keyword_index(docs))
+
+# Expected output: {'Hello': {0: 1}, 'world': {0: 1, 1: 1}, 'of': {1: 1}, 'python': {1: 1, 2: 1}, 'is': {2: 1}, 'a': {2: 1}, 'snake': {2: 1}}
