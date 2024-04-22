df482d522a3f9b88bed5cee7beda8f72eeb35d7d
Soumyabrata Kole <soumyakole@gmail.com>
2024-04-22 00:12:39 -0500

    Update Word_Index.py

--- Word_Index.py
+++ Word_Index.py
@@ -11,3 +11,9 @@ docs = ["Hello world", "world of python", "python is a snake"]
 print(keyword_index(docs))
 
 # Expected output: {'Hello': {0: 1}, 'world': {0: 1, 1: 1}, 'of': {1: 1}, 'python': {1: 1, 2: 1}, 'is': {2: 1}, 'a': {2: 1}, 'snake': {2: 1}}
+
+d = defaultdict(dict)
+for i,s in enumerate(docs):
+    for j, w in enumerate(s.split(' ')):
+        d[w][i]=j
+print(d)
